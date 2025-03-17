0447c21d0aa5641efd5d8e6a97aadf31b52ace4b
amchangama <mchangama.a05@gmail.com>
2025-03-17 17:41:55 +0530

    chore: Added the hindi language feature

--- src/app/Layout/home/home.component.ts
+++ src/app/Layout/home/home.component.ts
@@ -3,10 +3,11 @@ import { SearchModalComponent } from "../../Core/search-modal/search-modal.compo
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SearchData } from '../../Core/search-data';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
-  imports: [SearchModalComponent,FormsModule],
+  imports: [SearchModalComponent,FormsModule,TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/Layout/navbar/navbar.component.ts
+++ src/app/Layout/navbar/navbar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, TranslateModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  currentLanguage = 'en';
+  langText = 'English';
 
+  constructor(private translate: TranslateService) { 
+    this.currentLanguage = localStorage.getItem('language') || 'en';
+    this.translate.use(this.currentLanguage);
+    this.updateLangText();
+  }
+
+  toggleLanguage(lang: string) {
+    this.currentLanguage = lang;
+    this.translate.use(lang);
+    localStorage.setItem('language', lang);
+    this.updateLangText();
+  }
+
+  updateLangText() {
+    this.langText = this.currentLanguage === 'en' ? 'English' : 'हिन्दी';
+  }
 }
