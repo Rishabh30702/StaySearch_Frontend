fc239bf83369baef1f1360c00c726bff0e1ab67c
amchangama <mchangama.a05@gmail.com>
2025-04-10 10:45:17 +0530

    FIXED: Feedback API

--- src/app/Layout/hotelliers/feedback.service.ts
+++ src/app/Layout/hotelliers/feedback.service.ts
@@ -7,14 +7,15 @@ import { Feedback } from './feedback.modal';
 })
 export class FeedbackService {
   private testUrl = 'http://localhost:8080/api/feedbacks'; // Your actual endpoint
+  private baseAPI: string = "https://staysearchbackend.onrender.com/api/feedbacks";
 
   constructor(private http: HttpClient) {}
 
   getAllFeedbacks(): Observable<Feedback[]> {
-    return this.http.get<Feedback[]>(this.testUrl+"/getAllFeedbacks");
+    return this.http.get<Feedback[]>(this.baseAPI+"/getAllFeedbacks");
   }
 
   deleteFeedback(id: number, options = {}): Observable<any> {
-    return this.http.delete(this.testUrl+"/delete/"+id, options);
+    return this.http.delete(this.baseAPI+"/delete/"+id, options);
   }
 }
