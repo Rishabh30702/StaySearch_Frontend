5c240efa2de8130d027d865f7bf2f52acd653095
yadavaman588 <147317740+yadavaman588@users.noreply.github.com>
2025-05-03 15:21:57 +0530

    hotels fetched from api, rooms fetched,enabled add new room, edit and update

--- src/app/Layout/hotelliers/room.service.ts
+++ src/app/Layout/hotelliers/room.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { Room } from './room.modal';
 
 @Injectable({
@@ -8,13 +8,13 @@ import { Room } from './room.modal';
 })
 export class RoomService {
   private testURL = 'http://localhost:8080/api/hotelier';
-  private baseURL = 'https://staysearchbackend.onrender.com/api/hotelier';
+  private baseURL = 'https://staysearchbackend.onrender.com/v1/mine/rooms';
 
   constructor(private http: HttpClient) {}
 
-  getRooms(hotelId: number): Observable<Room[]> {
+  /*getRooms(hotelId: number): Observable<Room[]> {
     return this.http.get<Room[]>(`${this.baseURL}/hotels/${hotelId}/rooms`);
-  }
+  } */
 
   addRoomWithoutImage(hotelId: number, room: Room): Observable<Room> {
     const formData = new FormData();
@@ -23,17 +23,31 @@ export class RoomService {
   
     return this.http.post<Room>(`${this.baseURL}/hotels/${hotelId}/rooms`, formData);
   }
-
-  addRoom(hotelId: number, formData: FormData): Observable<Room> {
-    return this.http.post<Room>(`${this.baseURL}/hotels/${hotelId}/rooms`, formData);
+  addRoom(data: any): Observable<Room> {
+      const token = localStorage.getItem('token');
+    
+        if (!token) {
+          return throwError(() => new Error('No auth token found'));
+        }
+    
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        });
+  
+    return this.http.post<Room>(`${this.baseURL}`, data, { headers });
   }
 
+    getRooms(){
+      return this.http.get<Room[]>(`${this.baseURL}`);
+    }
+
   deleteRoom(roomId: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseURL}/rooms/${roomId}`);
+    
+    return this.http.delete<void>(`https://staysearchbackend.onrender.com/api/hotelier/rooms/${roomId}`);
   }
 
   updateRoom(roomId: number, roomData: FormData): Observable<Room> {
-    return this.http.put<Room>(`${this.baseURL}/rooms/${roomId}`, roomData);
+    return this.http.put<Room>(`https://staysearchbackend.onrender.com/api/hotelier/rooms/${roomId}`, roomData);
   }
 
 }
